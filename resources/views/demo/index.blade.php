da5ac1db134bbbf64ba5ca7e25c7c7d5e0c16f86
George Miller <github@duffleman.co.uk>
2015-05-21 19:42:56 +0100

    Getting ready to move to the new Pushman Instance

--- resources/views/demo/index.blade.php
+++ resources/views/demo/index.blade.php
@@ -257,7 +257,7 @@ function connect() {
 
 	// This token changes every 60 minutes automatically.
 	// We use Serverside helpers so we never have to change the code.
-	conn = new ab.Session('ws://pushman.dfl.mn:8080?token=zjVHWGbiI2fQjo1A2AQK',
+	conn = new ab.Session('ws://live.pushman.dfl.mn:8080?token=zjVHWGbiI2fQjo1A2AQK',
 		function() {
 			divLog('Connected to Pushman via Web Socket.');
 			conn.subscribe('restart_warning', function(topic, data) {
